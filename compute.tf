62e6e2f0bdbd9eed36cbab32049a30779d78f380
Neeraj Rana <neeraj825328rana@gmail.com>
2026-06-11 17:49:08 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -9,6 +9,8 @@ resource "aws_instance" "web" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+  key_name = aws_key_pair.deployer.key_name
+
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
@@ -28,6 +30,8 @@ resource "aws_instance" "db" {
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  
+  key_name = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "db-server"
